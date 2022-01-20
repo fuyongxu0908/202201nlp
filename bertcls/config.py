836395e7d169279c845b6c37199f2e9a5ee0e449
fyxu0908 <xfysec@163.com>
2022-01-20 15:59:43 +0800

    udpate config

--- bertcls/config.py
+++ bertcls/config.py
@@ -1,1 +1,7 @@
 import os
+import torch
+
+class Config(object):
+    def __init__(self, data_dir):
+        assert os.path.exists(data_dir)
+        self.train_file = os.path.join(data_dir, "train.txt")
